c129a2a093300f45873160605be9a48dec0321eb
Emmanuel Janssens <ejanssen@c2r5s7.42lausanne.ch>
2022-10-26 13:32:01 +0200

    substr itoa

--- libft/main.c
+++ libft/main.c
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ejanssen <ejanssen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 12:59:18 by ejanssen          #+#    #+#             */
-/*   Updated: 2022/10/26 13:31:07 by ejanssen         ###   ########.fr       */
+/*   Created: 2022/10/26 10:33:35 by ejanssen          #+#    #+#             */
+/*   Updated: 2022/10/26 13:26:50 by ejanssen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+int	main(int argc, char **argv)
 {
-	char	*sub;
-	char	*begin;
-	size_t	i;
+	char	*res;
+	char	str[] = "lorem ipsum dolor sit amet";
 
-	if (start + len > ft_strlen(s))
-		len = ft_strlen(s);
-	if (start > ft_strlen(s))
-		return (ft_strdup(""));
-	sub = malloc((len + 1) * sizeof (char));
-	if (sub == NULL)
-		return (NULL);
-	begin = sub;
-	i = 0;
-	while (i < len && (i + start) < ft_strlen(s))
-	{
-		*sub = s[i + start];
-		i++;
-		sub++;
-	}
-	*sub = '\0';
-	return ((char *)begin);
+	//res = ft_substr(str, atoi(argv[2]), atoi(argv[3]));
+	res = ft_substr("hola", 0, 18446744073709551615);
+	printf("%s %lu\n",res, ft_strlen(res));
+	free(res);
+	return (0);
 }
