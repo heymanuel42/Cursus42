a427ae9860e7cc7a3be528bdbb692b1dfa846e2f
Emmanuel Janssens <ejanssen@c2r5s7.42lausanne.ch>
2022-10-27 12:41:33 +0200

    finishing touches

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: ejanssen <ejanssen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 11:00:33 by ejanssen          #+#    #+#             */
-/*   Updated: 2022/10/25 13:05:48 by ejanssen         ###   ########.fr       */
+/*   Updated: 2022/10/27 12:39:38 by ejanssen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t nb)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	int		src_id;
 	size_t	dst_len;
@@ -23,16 +23,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t nb)
 	src_len = ft_strlen(src);
 	offset = dst_len;
 	src_id = 0;
-	if (nb == 0)
+	if (dstsize == 0)
 		return (src_len);
-	while (src[src_id] != '\0' && offset < nb - 1)
+	while (src[src_id] != '\0' && offset < dstsize - 1)
 	{
 		dst[offset] = src[src_id];
 		offset++;
 		src_id++;
 	}
 	dst[dst_len + src_id] = '\0';
-	if (nb < dst_len)
-		return (src_len + nb);
+	if (dstsize < dst_len)
+		return (src_len + dstsize);
 	return (dst_len + src_len);
 }
